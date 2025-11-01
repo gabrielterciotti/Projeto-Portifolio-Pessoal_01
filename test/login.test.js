1144e7a206e356b6b886ee8e70f4d051d4710c89
Gabriel <gabrielterciotti@gmail.com>
2025-11-01 11:15:08 -0300

    Adicionei testes automatizados de login

--- test/login.test.js
+++ test/login.test.js
@@ -7,12 +7,12 @@ const postLogin = require('../fixtures/postLogin.json')
 
 describe('Login', () => {
     describe('POST /auth/login', () => {
-        it('Deve retornar 200 com token em string quando usar credenciaisvalidas ', async () => { 
+        it('Deve retornar 200 com token em string quando usar credenciais validas ', async () => { 
             const bodyLogin ={ ...postLogin }
 
             const resposta = await request(process.env.BASE_URL) 
             
-                .post('/login')
+                .post('auth/login')
                 .set('Content-Type', 'application/json')
                 .send(bodyLogin)
 
@@ -22,6 +22,43 @@ describe('Login', () => {
 
 
         })
+
+        it('Deve retornar 401 sem o token em string quando usar senha invalida ', async () => { 
+            const bodyLogin ={ ...postLogin }
+
+            bodyLogin.password = "senhaErrada"
+
+            const resposta = await request(process.env.BASE_URL) 
+            
+                .post('auth/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin)
+
+            
+            expect(resposta.status).to.equal(401); 
+            expect(resposta.body.token).to.be.undefined;
+
+
+        })
+
+        it('Deve retornar 401 sem o token em string quando usar usuario invalido ', async () => { 
+            const bodyLogin ={ ...postLogin }
+
+            bodyLogin.username = "usuarioInvalido"
+
+            const resposta = await request(process.env.BASE_URL) 
+            
+                .post('auth/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin)
+
+            
+            expect(resposta.status).to.equal(401); 
+            expect(resposta.body.token).to.be.undefined;
+
+
+        })
+
 
     })
 
